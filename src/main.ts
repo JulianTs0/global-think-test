0d2b9a5d0d3cf06563f24524398d065496c9611f
JulianTs0 <juliantorresssr@gmail.com>
2026-02-10 10:46:55 -0300

    add: inject appConfigModule && add: port env to bootstrapt app

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { AppConfigService } from './config/app-config.service';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+    const app = await NestFactory.create(AppModule);
+
+    const config = app.get(AppConfigService);
+
+    await app.listen(config.appPort);
 }
 bootstrap();
