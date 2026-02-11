73e60493660e4c873abc30709cbfc8a94d2884c1
JulianTs0 <juliantorresssr@gmail.com>
2026-02-11 15:43:37 -0300

    add: users persistance layer

---
+++ src/users/persistance/index.ts
@@ -0,0 +1,6 @@
+export * from './repository/user.repository';
+
+// Datasource
+export * from './datasource/data/mongo/dao/mongo-user.dao';
+export * from './datasource/data/mongo/mapper/user-entity.mapper';
+export * from './datasource/data/mongo/schemas/user.model';
---
+++ src/users/persistance/repository/user.repository.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@nestjs/common';
+import { User } from 'src/commons';
+import { UserRepositoryI } from 'src/users/domain';
+import { MongoUserDao } from '../datasource/data/mongo/dao/mongo-user.dao';
+import { UserEntityMapper } from '../datasource/data/mongo/mapper/user-entity.mapper';
+import { UserModel } from '../datasource/data/mongo/schemas/user.model';
+
+/*
+ * Implementacion del del repositorio de usuarios, puede hablar tanto en
+ * idioma del negocio como en la bdd, gracias al UserEntityMapper, transforma
+ * dominio en bdd, y bdd en dominio, aplicando pequeña logica
+ */
+
+@Injectable()
+export class UserRepository implements UserRepositoryI {
+    constructor(private readonly dao: MongoUserDao) {}
+
+    public async findById(id: string): Promise<User | null> {
+        const model = await this.dao.findById(id);
+        return UserEntityMapper.toDomain(model);
+    }
+
+    public async findByEmail(email: string): Promise<User | null> {
+        const model = await this.dao.findByEmail(email);
+        return UserEntityMapper.toDomain(model);
+    }
+
+    public async save(user: User): Promise<User> {
+        const userModel: UserModel = UserEntityMapper.toModel(user)!;
+        const saved = await this.dao.save(userModel);
+        return UserEntityMapper.toDomain(saved)!;
+    }
+}
