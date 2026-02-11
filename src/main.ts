d791beb05a2f01a5b586a685b89f86ab75281bb9
JulianTs0 <juliantorresssr@gmail.com>
2026-02-11 15:44:16 -0300

    update: main module

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { AppConfigService } from './config/app-config.service';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
 
     const config = app.get(AppConfigService);
+
+    const swaggerConfig = new DocumentBuilder()
+        .setTitle('Prueba Tecnica de Global Think')
+        .setDescription('Documentación de la AP')
+        .setVersion('1.0')
+        .addBearerAuth()
+        .build();
+
+    const document = SwaggerModule.createDocument(app, swaggerConfig);
+    SwaggerModule.setup('api/docs', app, document);
 
     await app.listen(config.appPort);
 }
