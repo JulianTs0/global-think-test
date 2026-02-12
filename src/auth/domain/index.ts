312e52f3cb8e0c3e9725670920cf52071c0a91ca
JulianTs0 <juliantorresssr@gmail.com>
2026-02-12 16:29:16 -0300

    fix: auth domain index file

--- src/auth/domain/index.ts
+++ src/auth/domain/index.ts
@@ -1,15 +1,12 @@
-// DTOs - Auth
+export * from './dto/auth/mapper/auth.mapper';
+export * from './dto/auth/mapper/implementation/auth-user.mapper';
+export * from './dto/auth/mapper/implementation/login.mapper';
+export * from './dto/auth/mapper/implementation/register.mapper';
 export * from './dto/auth/request/auth.request.dto';
 export * from './dto/auth/request/login.request.dto';
 export * from './dto/auth/request/register.request.dto';
 export * from './dto/auth/response/auth.response.dto';
 export * from './dto/auth/response/login.response.dto';
 export * from './dto/auth/response/register.response.dto';
-export * from './dto/auth/mapper/auth.mapper';
-export * from './dto/auth/mapper/implementation/auth-user.mapper';
-export * from './dto/auth/mapper/implementation/login.mapper';
-export * from './dto/auth/mapper/implementation/register.mapper';
-
-// Services
 export * from './services/auth-service.interface';
 export * from './services/auth.service';
