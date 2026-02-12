4413732b2966e99a5dddb96342df1176f0f52a62
JulianTs0 <juliantorresssr@gmail.com>
2026-02-12 16:32:53 -0300

    add: new services features

--- src/users/domain/services/user.service.ts
+++ src/users/domain/services/user.service.ts
@@ -5,9 +5,11 @@ import { EditRes } from '../dto/users/response/edit.response.dto';
 import { GetByIdReq } from '../dto/users/request/get-by-id.request.dto';
 import { GetByIdRes } from '../dto/users/response/get-by-id.response.dto';
 import { UserServiceI } from './user-service.interface';
-import { User } from 'src/commons';
+import { Errors, PageContent, ServiceException, User } from 'src/commons';
 import { UserRepositoryI } from '../repository/user-repository.interface';
-
+import { SearchUsersReq } from '../dto/users/request/search-users.request.dto';
+import { SearchUsersRes } from '../dto/users/response/search-users.response.dto';
+import { UserMapper } from '../dto/users/mapper/user.mapper';
 /*
  * Implementacion del user service, ademas de tener los metodos de servicio
  * para exponer tiene metodos privados que solo se consumen en el authService
@@ -16,23 +18,74 @@ import { UserRepositoryI } from '../repository/user-repository.interface';
 
 @Injectable()
 export class UserService implements UserServiceI {
-    constructor(private readonly userRepository: UserRepositoryI) { }
+    constructor(private readonly userRepository: UserRepositoryI) {}
 
     public async getById(request: GetByIdReq): Promise<GetByIdRes> {
-        return Promise.resolve({} as GetByIdRes);
+        const user: User | null = await this.userRepository.findById(
+            request.id,
+        );
+
+        if (!user) {
+            throw new ServiceException(Errors.USER_NOT_FOUND);
+        }
+
+        return UserMapper.getById().toResponse(user);
     }
 
     public async delete(request: DeleteReq): Promise<void> {
-        return Promise.resolve();
+        const user: User | null = await this.userRepository.findById(
+            request.id,
+        );
+
+        if (!user) {
+            throw new ServiceException(Errors.USER_NOT_FOUND);
+        }
+
+        if (user.id !== request.authUser.id) {
+            throw new ServiceException(Errors.UNAUTHORIZED);
+        }
+
+        await this.userRepository.delete(user);
     }
 
     public async edit(request: EditReq): Promise<EditRes> {
-        return Promise.resolve({} as EditReq);
+        const user: User | null = await this.userRepository.findById(
+            request.id,
+        );
+        if (!user) {
+            throw new ServiceException(Errors.USER_NOT_FOUND);
+        }
+        if (user.id !== request.authUser.id) {
+            throw new ServiceException(Errors.UNAUTHORIZED);
+        }
+
+        user.fullName = request.body.fullName;
+        user.shortDescription = request.body.shortDescription;
+        user.email = request.body.email;
+        user.phoneNumber = request.body.phoneNumber;
+
+        const updated: User = await this.userRepository.update(
+            request.id,
+            user,
+        );
+
+        return UserMapper.edit().toResponse(updated!);
+    }
+
+    public async searchUsers(request: SearchUsersReq): Promise<SearchUsersRes> {
+        let userPage: PageContent<User> = await this.userRepository.findAll(
+            request.size,
+            request.page,
+            request.fullName,
+        );
+
+        return UserMapper.searchUsers().toResponse(userPage);
     }
 
     public async register(user: User): Promise<User> {
-        const saved: User = await this.userRepository.save(user);
-        return Promise.resolve(saved);
+        const saved: User | null = await this.userRepository.save(user);
+
+        return saved;
     }
 
     public async findById(id: string): Promise<User | null> {
@@ -42,6 +95,7 @@ export class UserService implements UserServiceI {
 
     public async findByEmail(email: string): Promise<User | null> {
         const user: User | null = await this.userRepository.findByEmail(email);
+
         return user;
     }
 }
