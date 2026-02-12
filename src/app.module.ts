dc34370b881297c043b5c553c9aa30be3a0e9c59
JulianTs0 <juliantorresssr@gmail.com>
2026-02-12 16:33:57 -0300

    fix: main imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,7 +4,7 @@ import { DatabaseModule } from './database/database.module';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { APP_FILTER, APP_PIPE } from '@nestjs/core';
-import { GlobalConstraintHandler, GlobalExceptionHandler } from './commons';
+import { GlobalConstraintHandler, GlobalExceptionHandler } from 'src/commons';
 
 @Module({
     imports: [AppConfigModule, DatabaseModule, AuthModule, UsersModule],
@@ -20,4 +20,4 @@ import { GlobalConstraintHandler, GlobalExceptionHandler } from './commons';
         },
     ],
 })
-export class AppModule { }
+export class AppModule {}
