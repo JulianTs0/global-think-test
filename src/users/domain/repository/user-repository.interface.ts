7c5409e41f77d3fdb7388c2082723bc5840e1d56
JulianTs0 <juliantorresssr@gmail.com>
2026-02-12 16:32:29 -0300

    fix: repository interface

--- src/users/domain/repository/user-repository.interface.ts
+++ src/users/domain/repository/user-repository.interface.ts
@@ -1,4 +1,4 @@
-import { User } from 'src/commons';
+import { PageContent, User } from 'src/commons';
 
 // Interfaz para el uso de repositorios de usuarios
 
@@ -6,4 +6,11 @@ export abstract class UserRepositoryI {
     abstract save(user: User): Promise<User>;
     abstract findById(id: string): Promise<User | null>;
     abstract findByEmail(email: string): Promise<User | null>;
+    abstract findAll(
+        size: number,
+        page: number,
+        name?: string,
+    ): Promise<PageContent<User>>;
+    abstract update(id: string, user: User): Promise<User>;
+    abstract delete(user: User): Promise<void>;
 }
