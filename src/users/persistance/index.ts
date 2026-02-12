14ec526f6ba5407c63f03c78088f12ed4600e7db
JulianTs0 <juliantorresssr@gmail.com>
2026-02-12 16:31:04 -0300

    fix: barrel file

--- src/users/persistance/index.ts
+++ src/users/persistance/index.ts
@@ -1,6 +1,4 @@
-export * from './repository/user.repository';
-
-// Datasource
 export * from './datasource/data/mongo/dao/mongo-user.dao';
 export * from './datasource/data/mongo/mapper/user-entity.mapper';
 export * from './datasource/data/mongo/schemas/user.model';
+export * from './repository/user.repository';
