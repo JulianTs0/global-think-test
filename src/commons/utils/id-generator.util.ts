2c04757ab7617e30fc697f59742bbbc548efe888
JulianTs0 <juliantorresssr@gmail.com>
2026-02-12 21:46:27 -0300

    fix: minnor fixes

--- src/commons/utils/id-generator.util.ts
+++ src/commons/utils/id-generator.util.ts
@@ -1,5 +1,7 @@
+import * as crypto from 'node:crypto';
+
 export class IdGenerator {
-    public constructor() {}
+    public constructor() { }
 
     public static generateUUID(): string {
         return crypto.randomUUID();
