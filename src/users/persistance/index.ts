e48da44b964fac8d81f39f272d6f4360f0b33e68
JulianTs0 <juliantorresssr@gmail.com>
2026-02-12 23:52:22 -0300

    fix: index.ts

--- src/users/persistance/index.ts
+++ src/users/persistance/index.ts
@@ -2,3 +2,7 @@ export * from './datasource/data/mongo/dao/mongo-user.dao';
 export * from './datasource/data/mongo/mapper/user-entity.mapper';
 export * from './datasource/data/mongo/schemas/user.model';
 export * from './repository/user.repository';
+export * from './datasource/data/mongo/dao/mongo-profile.dao';
+export * from './datasource/data/mongo/mapper/profile-entity.mapper';
+export * from './datasource/data/mongo/schemas/profile.model';
+export * from './repository/profile.repository';
